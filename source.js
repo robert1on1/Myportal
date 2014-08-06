914264be89a3e32881b5fa54093570998e333eb9
Kyamanywa Robert <mbabazirobert@gmail.com>
2014-08-06 14:48:25 +0300

    added favicon and js file
    new favicon file added and source js file with code,

---
+++ source.js
@@ -0,0 +1,202 @@
+var hoverEffect = true; // set true for hover effect, set false for no hover effect
+
+var searchEngine = 'google'; // default search engine - set google for google search, bing for bing search, yahoo for yahoo search
+
+var numberOfScreens = 3; // set number of screens (1 or 2 or 3)
+ 
+var blockName = new Array(); // set names of blocks
+blockName[1] = 'Welcome to Africa One ePortal';
+blockName[2] = 'News | Business | Directory & Jobs';
+blockName[3] = 'Sports | Life | Entertaiment | Culture & fun';
+
+var bookmark = new Array();
+bookmark[0] = new Array();
+bookmark[1] = new Array();
+bookmark[2] = new Array();
+
+// set your bookmarks here: (If you do not fill 'thumb' for thumbnail will be used title)
+// FIRST BLOCK
+bookmark[0][0] = {
+	'title':'YouTube',
+	'url':'#',
+	'thumb':'youtube.png'
+};
+bookmark[0][1] = {
+	'title':'Yahoo',
+	'url':'#',
+	'thumb':'yahoo.png'
+};
+bookmark[0][2] = {
+	'title':'Grooveshark',
+	'url':'#',
+	'thumb':'grooveshark.png'
+};
+bookmark[0][3] = {
+	'title':'last.fm',
+	'url':'#',
+	'thumb':'lastfm.png'
+};
+bookmark[0][4] = {
+	'title':'twitter',
+	'url':'#',
+	'thumb':'twitter.png'
+};
+bookmark[0][5] = {
+	'title':'Africa One',
+	'url':'#',
+	'thumb':'a1.png'
+};
+bookmark[0][6] = {
+	'title':'facebook',
+	'url':'#',
+	'thumb':'facebook.png'
+};
+bookmark[0][7] = {
+	'title':'BBC news',
+	'url':'http://www.bbc.co.uk/news/',
+	'thumb':'bbcnews.png'
+};
+bookmark[0][8] = {
+	'title':'CNN',
+	'url':'http://www.cnn.com',
+	'thumb':'cnn.png'
+};
+bookmark[0][9] = {
+	'title':'deviantART',
+	'url':'#',
+	'thumb':'deviantart.png'
+};
+bookmark[0][10] = {
+	'title':'wikipedia',
+	'url':'#',
+	'thumb':'wikipedia.png'
+};
+bookmark[0][11] = {
+	'title':'iTunes',
+	'url':'#',
+	'thumb':'itunes.png'
+};
+// end of FIRST BLOCK
+// SECOND BLOCK
+bookmark[1][0] = {
+	'title':'linkedin',
+	'url':'#',
+	'thumb':'linkedin.png'
+};
+bookmark[1][1] = {
+	'title':'digg',
+	'url':'#',
+	'thumb':'digg.png'
+};
+bookmark[1][2] = {
+	'title':'flickr',
+	'url':'#',
+	'thumb':'flickr.png'
+};
+bookmark[1][3] = {
+	'title':'msn',
+	'url':'#',
+	'thumb':'msn.png'
+};
+bookmark[1][4] = {
+	'title':'reddit',
+	'url':'#',
+	'thumb':'reddit.png'
+};
+bookmark[1][5] = {
+	'title':'skype',
+	'url':'#',
+	'thumb':'skype.png'
+};
+bookmark[1][6] = {
+	'title':'technorati',
+	'url':'#',
+	'thumb':'technorati.png'
+};
+bookmark[1][7] = {
+	'title':'delicious',
+	'url':'#',
+	'thumb':'delicious.png'
+};
+bookmark[1][8] = {
+	'title':'MySpace',
+	'url':'#',
+	'thumb':'myspace.png'
+};
+bookmark[1][9] = {
+	'title':'Directory',
+	'url':'#',
+	'thumb':'orkut.png'
+};
+bookmark[1][10] = {
+	'title':'tumblr',
+	'url':'#',
+	'thumb':'tumblr.png'
+};
+bookmark[1][11] = {
+	'title':'StumbleUpon',
+	'url':'#',
+	'thumb':'stumbleupon.png'
+};
+//end of SECOND BLOCK
+// THIRD BLOCK
+bookmark[2][0] = {
+	'title':'eurosport',
+	'url':'#',
+	'thumb':'eurosport.png'
+};
+bookmark[2][1] = {
+	'title':'Facebook',
+	'url':'#',
+	'thumb':'amazon.png'
+};
+bookmark[2][2] = {
+	'title':'eBay',
+	'url':'#',
+	'thumb':'ebay.png'
+};
+bookmark[2][3] = {
+	'title':'IMDb',
+	'url':'#',
+	'thumb':'imdb.png'
+};
+bookmark[2][4] = {
+	'title':'vimeo',
+	'url':'http://vimeo.com',
+	'thumb':'vimeo.png'
+};
+bookmark[2][5] = {
+	'title':'Fashion',
+	'url':'#',
+	'thumb':'lifehacker.png'
+};
+bookmark[2][6] = {
+	'title':'engadged',
+	'url':'#',
+	'thumb':'engadget.png'
+};
+bookmark[2][7] = {
+	'title':'zune',
+	'url':'#',
+	'thumb':'zune.png'
+};
+bookmark[2][8] = {
+	'title':'dropbox',
+	'url':'#',
+	'thumb':'dropbox.png'
+};
+bookmark[2][9] = {
+	'title':'National Geographic',
+	'url':'#',
+	'thumb':'natgeo.png'
+};
+bookmark[2][10] = {
+	'title':'Talent',
+	'url':'#',
+	'thumb':'cbcnews.png'
+};
+bookmark[2][11] = {
+	'title':'weather.com',
+	'url':'#',
+	'thumb':'weather.png'
+};
